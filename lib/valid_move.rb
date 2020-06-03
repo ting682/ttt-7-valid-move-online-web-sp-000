c3e9477555af3cce79eef0bd671f568b09eaf694
Ting Chung <tchung682@gmail.com>
2020-06-03 19:05:50 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,11 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
 
-  if position_taken?(board, index) && index.between(0,8)
+  if position_taken?(board, index) && index.between(0,8) && board == [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+    true
+
+  elsif position_taken?(board, index) && index.between(0,8) 
 
     true
 
